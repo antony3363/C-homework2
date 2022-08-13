7d4ed439b3fdee63199ca3adc4f897e809f3bc1a
Anton <anton_artemjev06@mail.ru>
2022-08-13 22:33:30 +0700

    ДЗ2

---
+++ exercise13/Program.cs
@@ -0,0 +1,44 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+
+System.Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+
+if(a < 100)
+    {
+        System.Console.WriteLine("третьей цифры нет");
+    }
+else
+{
+    
+    if (a % 100 < 10)
+    {
+        System.Console.WriteLine(a);
+    }
+    else
+    {
+    while (a > 999)
+        {
+            a = a / 10;
+            
+        } 
+        a = a % 10; 
+        System.Console.WriteLine(a);
+    }    
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ exercise15/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+System.Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+if (a == 6 ^ a == 7)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
